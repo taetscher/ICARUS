731cb827d6fc2f389db9ec71c71bae0091aa3226
Benjamin Schüpbach <auteblauwau@hotmail.com>
2019-05-04 16:25:11 +0200

    ICARUS Viewer fully functional
    ICARUS Viewer is now fully functional

--- imageClassification/ICARUS/darkFLOW_repo/darkflow-master/ICARUSViewer.py
+++ imageClassification/ICARUS/darkFLOW_repo/darkflow-master/ICARUSViewer.py
@@ -9,7 +9,7 @@ infile_name = "georefMediaTweets2019-05-03.csv"
 
 in_file = folder_path + infile_name
 
-
+#Viewer magic
 with open(in_file) as fp:
     line = fp.readline()
 
@@ -19,19 +19,16 @@ with open(in_file) as fp:
             array = line.split("; ")
 
 
+
+
             # extract coordinate information (lat/long), link to media, timestamp, uuid
             coord_x = array[0]
             coord_y = array[1]
             medurl = array[2]
             timestamp = array[3]
             avr_confidence = array[4]
-            result = array[5]
+            result = eval(array[5])
 
-            tl = (eval(result)[0]['topleft']['x'], eval(result)[0]['topleft']['y'])
-            br = (eval(result)[0]['bottomright']['x'], eval(result)[0]['bottomright']['y'])
-            label = eval(result)[0]['label']
-
-            print("Avr. confidence: ", avr_confidenceq)
 
             # download image, if this fails, go with next one
             try:
@@ -48,9 +45,55 @@ with open(in_file) as fp:
 
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-                img = cv2.rectangle(img, tl, br, (0, 255, 0), 7)
-                img = cv2.putText(img, "ASR", tl, cv2.FONT_HERSHEY_PLAIN,1, (0, 0, 0), 2)
+                for x in range(0,len(result)):
+                    #get parameters that you need to display image
+                    tl = result[x]['topleft']['x'], result[x]['topleft']['y']
+                    br = result[x]['bottomright']['x'], result[x]['bottomright']['y']
+                    confidence = result[x]['confidence']
+                    label = "ASR: " + str(confidence)[:4]
 
+                    #prepare display of text
+                    fontface = cv2.FONT_HERSHEY_PLAIN
+                    fontscale = 2
+                    thickness = 2
+                    predict_color = (0, 255, 0)
+                    predict_thickness = 3
+                    font_color = (255, 255, 255)
+                    background_color = (0, 0, 0)
+
+                    label_size = cv2.getTextSize(label, fontface, fontscale, thickness)
+
+                    #determine where to put label
+                    height, width = img.shape[:2]
+                    print(height, width)
+
+                    if tl[0] > width/2:
+                        #put label left of predict if it were off-screen otherwise
+                        text_width = label_size[0][0]
+                        text_height = label_size[0][1] + 5
+
+                        label_tl = (tl[0]-text_width, tl[1] - 10)
+                        label_br = (label_tl[0] + text_width, label_tl[1] - text_height)
+
+
+                    else:
+                        # put label on top of predict for all other cases
+                        text_width = label_size[0][0]
+                        text_height = label_size[0][1] + 5
+
+                        label_tl = (tl[0], tl[1] - 10)
+                        label_br = (label_tl[0] + text_width, label_tl[1] - text_height)
+
+                    #draw rectangle for prediction
+                    img = cv2.rectangle(img, tl, br, predict_color, predict_thickness)
+                    #draw background for label
+                    img = cv2.rectangle(img, label_tl, label_br, background_color, -1)
+                    #draw label
+                    img = cv2.putText(img, label , label_tl, fontface , fontscale, font_color, thickness)
+
+                #make plt show the image fullscreen
+                mng = plt.get_current_fig_manager()
+                mng.window.state('zoomed')
                 plt.imshow(img)
                 plt.show()
 
