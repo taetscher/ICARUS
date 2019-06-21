bd30637ec8432e07f1bf5df6102c855a564d85a3
Benjamin Schüpbach <auteblauwau@hotmail.com>
2019-06-21 17:37:13 +0200

    Update to ISS tracker

--- imageClassification/ICARUS/darkFLOW_repo/darkflow-master/ISStracker.py
+++ imageClassification/ICARUS/darkFLOW_repo/darkflow-master/ISStracker.py
@@ -3,5 +3,8 @@ import requests
 while True:
     iss_info = requests.get('https://api.wheretheiss.at/v1/satellites/25544').content
     with open('Savefiles/ISS_Info.txt', 'a') as logger:
-        logger.write(str(iss_info))
-    print(iss_info)
+        line = eval(str(iss_info)[2:-1])
+        logger.write(str(line) + "\n")
+        
+        print(line)
+        
